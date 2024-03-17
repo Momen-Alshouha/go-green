f745ed6c843a3dea4a73dfb09d3820a789d74df9
momen-alshouha <momenalshouha@outlook.com>
2024-03-17 19:26:08 +0300

    support , contact , home , about components generated and navbar done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 
-export const routes: Routes = [];
+// lazy-load standalone component
+export const routes: Route[] = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/home.component').then((mod) => mod.HomeComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./about/about.component').then((mod) => mod.AboutComponent),
+  },
+  {
+    path: 'support',
+    loadComponent: () =>
+      import('./support/support.component').then((mod) => mod.SupportComponent),
+  },
+  {
+    path:'contact',
+    loadComponent:()=> 
+    import('./contact/contact.component').then((mod)=> mod.ContactComponent)
+  }
+];
