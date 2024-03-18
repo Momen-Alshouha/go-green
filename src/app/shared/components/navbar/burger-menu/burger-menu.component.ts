3226f56f86176e1dd343786e453d3f1012ad54e3
momen-alshouha <momenalshouha@outlook.com>
2024-03-19 01:21:42 +0300

    burger menu done

--- src/app/shared/components/navbar/burger-menu/burger-menu.component.ts
+++ src/app/shared/components/navbar/burger-menu/burger-menu.component.ts
@@ -1,21 +1,32 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  Output,
+} from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-burger-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './burger-menu.component.html',
-  styleUrl: './burger-menu.component.scss'
+  styleUrl: './burger-menu.component.scss',
 })
 export class BurgerMenuComponent {
-  
   @Output() opened = new EventEmitter<any>();
-  active :boolean = false;
+  active: boolean = false;
 
-  ngOnInit() {
-   
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.active = false;
+      }
+    });
   }
-
   onBurgerClicked() {
     this.active = !this.active;
     this.opened.emit();
